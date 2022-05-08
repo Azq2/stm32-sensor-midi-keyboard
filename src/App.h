d46b14c615fff144aa050c090ea6526518fa85c5
Kirill Zhumarin <Azq2@users.noreply.github.com>
2022-05-09 01:44:50 +0300

    minor fixes

--- src/App.h
+++ src/App.h
@@ -8,8 +8,8 @@ class App {
 	protected:
 		// Config here
 		constexpr static bool DEBUG_KEYS = false; // true for key press debug
-		constexpr static char NOTE_OFFSET = 36; // C2
-		constexpr static uint32_t DEBOUNCE_CYCLES = 10000000; // cpu cycles for debounce keys
+		constexpr static char NOTE_OFFSET = 48; // C2
+		constexpr static uint32_t DEBOUNCE_CYCLES = 5000000; // cpu cycles for debounce keys
 		constexpr static float SENSOR_KEY_THRESHOLD = 1; // Threshold in % for sensor key "pressed" state
 	public:
 		struct Pin {
